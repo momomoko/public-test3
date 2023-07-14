fdad10936208663ea613b2d34faffba3507b2b65
c2489 <136797367+momomoko@users.noreply.github.com>
2023-07-14 18:48:38 +0900

    Use public-test3

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/public-test2/',
+  base: '/public-test3/',
   plugins: [react()],
 })
